76c2e91646b07fde99edaf1bf7589a58056d75e7
pb319 <pranaybiswas319@gmail.com>
2024-10-13 22:46:32 +0530

    Model Loaded

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI, File, UploadFile
 import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+
+
+MODEL = tf.keras.models.load_model("/home/prasun/GitDemo/Potato_DeepDeploy/1.keras")
+CLASS_NAMES = ['Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy']
 
 #Instantiating Class 
 app = FastAPI()
@@ -8,9 +16,25 @@ app = FastAPI()
 async def ping():
     return "FastApi Server Running"
 
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
-    pass
+    
+    #Reading The File
+    image = read_file_as_image(await file.read())
+    image_batch = np.expand_dims(image,0)
+    predictions = MODEL.predict(image_batch)
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+
+    return {
+        "predicted_class": predicted_class,
+        "confidence": float(confidence)
+    }
   
 
 if __name__ == "__main__":
